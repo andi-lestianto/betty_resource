7c9ff5c6bd0a10c038d3c40020fce910df673e05
andi-lestianto <andi7les@gmail.com>
2024-12-10 16:48:23 +0700

    update generalutis

--- lib/assetsutils/general/generalassets_utils.dart
+++ lib/assetsutils/general/generalassets_utils.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 class GeneralAssetsUtils {
-  static const String logo = 'assets/bettygeneral/bettylogo.png';
+  static const String logo =
+      'packages/betty_resource/assets/bettygeneral/bettylogo.png';
   static const Color color = Color(0xFF4C4C4C);
 }
