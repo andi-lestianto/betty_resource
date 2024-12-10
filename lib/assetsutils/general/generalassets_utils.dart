f47ff54082c64e253b84f4577614123436e94f20
andi-lestianto <andi7les@gmail.com>
2024-12-10 16:39:27 +0700

    init

---
+++ lib/assetsutils/general/generalassets_utils.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class GeneralAssetsUtils {
+  static const String logo = 'assets/bettygeneral/bettylogo.png';
+  static const Color color = Color(0xFF4C4C4C);
+}
---
+++ lib/betty_resource.dart
@@ -0,0 +1,7 @@
+library betty_resource;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
