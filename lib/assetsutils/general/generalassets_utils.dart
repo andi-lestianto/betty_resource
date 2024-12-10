f292b373ca72fd85d8cbabcfe3871f179bf4b081
andi-lestianto <andi7les@gmail.com>
2024-12-10 18:08:42 +0700

    add font

--- lib/assetsutils/general/generalassets_utils.dart
+++ lib/assetsutils/general/generalassets_utils.dart
@@ -4,4 +4,7 @@ class GeneralAssetsUtils {
   static const String logo =
       'packages/betty_resource/assets/bettygeneral/bettylogo.png';
   static const Color color = Color(0xFF4C4C4C);
+
+  static TextStyle? textStyle =
+      TextStyle(fontFamily: 'Quicksand', color: color);
 }
