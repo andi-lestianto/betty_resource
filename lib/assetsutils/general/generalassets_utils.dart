24e8db4e3f2c9634cc6f3a1643aabe46d7d3b64d
andi-lestianto <andi7les@gmail.com>
2024-12-10 18:57:50 +0700

    update lagi

--- lib/assetsutils/general/generalassets_utils.dart
+++ lib/assetsutils/general/generalassets_utils.dart
@@ -1,10 +1,10 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 class GeneralAssetsUtils {
   static const String logo =
       'packages/betty_resource/assets/bettygeneral/bettylogo.png';
   static const Color color = Color(0xFF4C4C4C);
 
-  static TextStyle? textStyle =
+  static const TextStyle textStyle =
       TextStyle(fontFamily: 'Quicksand', color: color);
 }
