b2d80f603dfd2ac008e29a7032c551c6b2e1d2b6
andi-lestianto <andi7les@gmail.com>
2024-12-11 21:52:44 +0700

    style : added icon & image utils

---
+++ lib/iconutils/bettyicon_utils.dart
@@ -0,0 +1,120 @@
+class BettyIcon {
+  static const String path = 'packages/betty_resource/';
+
+  static const bettySvg = '${path}assets/icon/betty.svg';
+  static const icAddSquareSvg = '${path}assets/icon/ic-add-square.svg';
+  static const icAddSquareActiveSvg =
+      '${path}assets/icon/ic-add-square_active.svg';
+  static const icAddSvg = '${path}assets/icon/ic-add.svg';
+  static const icArchiveTickSvg = '${path}assets/icon/ic-archive-tick.svg';
+  static const icArrowCircleRightSvg =
+      '${path}assets/icon/ic-arrow-circle-right.svg';
+  static const icArrowKeyboardSvg = '${path}assets/icon/ic-arrow-keyboard.svg';
+  static const icArrowRightSvg = '${path}assets/icon/ic-arrow-right.svg';
+  static const icArrowSortSvg = '${path}assets/icon/ic-arrow-sort.svg';
+  static const icArrowSvg = '${path}assets/icon/ic-arrow.svg';
+  static const icAttachmentSvg = '${path}assets/icon/ic-attachment.svg';
+  static const icBackupSvg = '${path}assets/icon/ic-backup.svg';
+  static const icBagHappySvg = '${path}assets/icon/ic-bag-happy.svg';
+  static const icBluetoothSvg = '${path}assets/icon/ic-bluetooth.svg';
+  static const icBoxTickActiveSvg = '${path}assets/icon/ic-box-tick-active.svg';
+  static const icBoxTickSvg = '${path}assets/icon/ic-box-tick.svg';
+  static const icCallCallingSvg = '${path}assets/icon/ic-call-calling.svg';
+  static const icCameraSvg = '${path}assets/icon/ic-camera.svg';
+  static const icCashSvg = '${path}assets/icon/ic-cash.svg';
+  static const icCategoryActiveSvg =
+      '${path}assets/icon/ic-category-active.svg';
+  static const icCategorySvg = '${path}assets/icon/ic-category.svg';
+  static const icChartSquareActiveSvg =
+      '${path}assets/icon/ic-chart-square-active.svg';
+  static const icChartSquareSvg = '${path}assets/icon/ic-chart-square.svg';
+  static const icCheckSvg = '${path}assets/icon/ic-check.svg';
+  static const icChecklistSvg = '${path}assets/icon/ic-checklist.svg';
+  static const icChooseImageSvg = '${path}assets/icon/ic-choose-image.svg';
+  static const icCleartextfieldSvg = '${path}assets/icon/ic-cleartextfield.svg';
+  static const icClipboardTickActiveSvg =
+      '${path}assets/icon/ic-clipboard-tick-active.svg';
+  static const icClipboardTickSvg = '${path}assets/icon/ic-clipboard-tick.svg';
+  static const icCloseCircleSvg = '${path}assets/icon/ic-close-circle.svg';
+  static const icCloseRoundedSvg = '${path}assets/icon/ic-close-rounded.svg';
+  static const icCloseSvg = '${path}assets/icon/ic-close.svg';
+  static const icDebitccSvg = '${path}assets/icon/ic-debitcc.svg';
+  static const icDeliveryActiveSvg =
+      '${path}assets/icon/ic-delivery-active.svg';
+  static const icDeliverySvg = '${path}assets/icon/ic-delivery.svg';
+  static const icDocumentDownloadSvg =
+      '${path}assets/icon/ic-document-download.svg';
+  static const icDocumentExportSvg =
+      '${path}assets/icon/ic-document-export.svg';
+  static const icDocumentSvg = '${path}assets/icon/ic-document.svg';
+  static const icEditSvg = '${path}assets/icon/ic-edit.svg';
+  static const icExcelSvg = '${path}assets/icon/ic-excel.svg';
+  static const icExpandSvg = '${path}assets/icon/ic-expand.svg';
+  static const icExportReportExcelSvg =
+      '${path}assets/icon/ic-export-report-excel.svg';
+  static const icExportReportPdfSvg =
+      '${path}assets/icon/ic-export-report-pdf.svg';
+  static const icExportReportSvg = '${path}assets/icon/ic-export-report.svg';
+  static const icEyeSlashSvg = '${path}assets/icon/ic-eye-slash.svg';
+  static const icEyeSvg = '${path}assets/icon/ic-eye.svg';
+  static const icGallerySvg = '${path}assets/icon/ic-gallery.svg';
+  static const icHomeSvg = '${path}assets/icon/ic-home.svg';
+  static const icInfoCircleSvg = '${path}assets/icon/ic-info-circle.svg';
+  static const icInstoreActiveSvg = '${path}assets/icon/ic-instore-active.svg';
+  static const icInstoreSvg = '${path}assets/icon/ic-instore.svg';
+  static const icKeySvg = '${path}assets/icon/ic-key.svg';
+  static const icLanguageSquareSvg =
+      '${path}assets/icon/ic-language-square.svg';
+  static const icLocationSvg = '${path}assets/icon/ic-location.svg';
+  static const icLoginSvg = '${path}assets/icon/ic-login.svg';
+  static const icLogoutSvg = '${path}assets/icon/ic-logout.svg';
+  static const icMapSvg = '${path}assets/icon/ic-map.svg';
+  static const icMaximizeSvg = '${path}assets/icon/ic-maximize.svg';
+  static const icMinSvg = '${path}assets/icon/ic-min.svg';
+  static const icMoneySendSvg = '${path}assets/icon/ic-money-send.svg';
+  static const icMotorSvg = '${path}assets/icon/ic-motor.svg';
+  static const icNoteActiveSvg = '${path}assets/icon/ic-note-active.svg';
+  static const icNoteSvg = '${path}assets/icon/ic-note.svg';
+  static const icNotificationSvg = '${path}assets/icon/ic-notification.svg';
+  static const icPdfSvg = '${path}assets/icon/ic-pdf.svg';
+  static const icPrinterSvg = '${path}assets/icon/ic-printer.svg';
+  static const icProductActiveSvg = '${path}assets/icon/ic-product-active.svg';
+  static const icProductSvg = '${path}assets/icon/ic-product.svg';
+  static const icProfileSvg = '${path}assets/icon/ic-profile.svg';
+  static const icQrisSvg = '${path}assets/icon/ic-qris.svg';
+  static const icQuestionMarkActiveSvg =
+      '${path}assets/icon/ic-question-mark-active.svg';
+  static const icQuestionMarkSvg = '${path}assets/icon/ic-question-mark.svg';
+  static const icRefreshSquare_2Svg =
+      '${path}assets/icon/ic-refresh-square-2.svg';
+  static const icRemovePaymentSvg = '${path}assets/icon/ic-remove-payment.svg';
+  static const icRotateRightSvg = '${path}assets/icon/ic-rotate-right.svg';
+  static const icRoutingSvg = '${path}assets/icon/ic-routing.svg';
+  static const icSearchNormalSvg = '${path}assets/icon/ic-search-normal.svg';
+  static const icSearchNotffoundSvg =
+      '${path}assets/icon/ic-search-notffound.svg';
+  static const icSetting_2Svg = '${path}assets/icon/ic-setting-2.svg';
+  static const icSettingSvg = '${path}assets/icon/ic-setting.svg';
+  static const icShareSvg = '${path}assets/icon/ic-share.svg';
+  static const icShieldSearchSvg = '${path}assets/icon/ic-shield-search.svg';
+  static const icShieldTickSvg = '${path}assets/icon/ic-shield-tick.svg';
+  static const icShopSvg = '${path}assets/icon/ic-shop.svg';
+  static const icShoppingNewSvg = '${path}assets/icon/ic-shopping-new.svg';
+  static const icShoppingSvg = '${path}assets/icon/ic-shopping.svg';
+  static const icSmsSvg = '${path}assets/icon/ic-sms.svg';
+  static const icStarSvg = '${path}assets/icon/ic-star.svg';
+  static const icTablewareSvg = '${path}assets/icon/ic-tableware.svg';
+  static const icToggleOffSvg = '${path}assets/icon/ic-toggle-off.svg';
+  static const icToggleOnSvg = '${path}assets/icon/ic-toggle-on.svg';
+  static const icToogleSvg = '${path}assets/icon/ic-toogle.svg';
+  static const icTransactionMinusActiveSvg =
+      '${path}assets/icon/ic-transaction-minus-active.svg';
+  static const icTransactionMinusSvg =
+      '${path}assets/icon/ic-transaction-minus.svg';
+  static const icTrashSvg = '${path}assets/icon/ic-trash.svg';
+  static const icUpdateSvg = '${path}assets/icon/ic-update.svg';
+  static const icUserAddSvg = '${path}assets/icon/ic-user-add.svg';
+  static const icUserTagSvg = '${path}assets/icon/ic-user-tag.svg';
+  static const icWarningSvg = '${path}assets/icon/ic-warning.svg';
+  static const starSvg = '${path}assets/icon/star.svg';
+}
---
+++ lib/imageutils/bettyimage_utils.dart
@@ -0,0 +1,29 @@
+class BettyImage {
+  static const String path = 'packages/betty_resource/';
+
+  static const imgEllips1Png = '${path}assets/image/img-ellips1.png';
+  static const imgEllips2Png = '${path}assets/image/img-ellips2.png';
+  static const imgGooglePlayPng = '${path}assets/image/img-googleplay.png';
+  static const imgMaincartGreenDotPng =
+      '${path}assets/image/img-maincart-greendot.png';
+  static const imgBoxReminderPng = '${path}assets/image/img-box-reminder.png';
+  static const imgPaymentGreenDotPng =
+      '${path}assets/image/img-payment-greendot.png';
+  static const imgPaymentOrangeDotPng =
+      '${path}assets/image/img-payment-orangedot.png';
+  static const imgTexturePng = '${path}assets/image/img-texture.png';
+  static const imgOnboardingGreenDotPng =
+      '${path}assets/image/img-onboarding-greendot.png';
+  static const imgTutorialDialogBgIntroPng =
+      '${path}assets/image/img-tutorialdialog-bgintro.png';
+  static const imgTutorialDialogBlackBottomPng =
+      '${path}assets/image/img-tutorialdialog-black-bottom.png';
+  static const imgTutorialDialogBlackLeftPng =
+      '${path}assets/image/img-tutorialdialog-black-left.png';
+  static const imgTutorialDialogBlackRightPng =
+      '${path}assets/image/img-tutorialdialog-black-right.png';
+  static const imgTutorialDialogBottomLeftPng =
+      '${path}assets/image/img-tutorialdialog-bottom-left.png';
+  static const imgTutorialDialogTopRightPng =
+      '${path}assets/image/img-tutorialdialog-top-right.png';
+}
