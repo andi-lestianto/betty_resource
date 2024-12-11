2e63d07167319568d10f5a5f99fa69f8605c3312
andi-lestianto <andi7les@gmail.com>
2024-12-11 16:44:27 +0700

    fix : font setup betty general

--- lib/betty_resource.dart
+++ lib/betty_resource.dart
@@ -1,9 +1,3 @@
 library betty_resource;
 
 export 'package:betty_resource/assetsutils/general/generalassets_utils.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
---
+++ lib/fontutils/bettyfont_utils.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class FontQuicksand {
+  static const TextStyle base =
+      TextStyle(fontFamily: 'Quicksand', package: 'betty_resource');
+
+  static TextStyle light = base.copyWith(fontWeight: FontWeight.w300);
+  static TextStyle regular = base.copyWith(fontWeight: FontWeight.w400);
+  static TextStyle medium = base.copyWith(fontWeight: FontWeight.w500);
+  static TextStyle semiBold = base.copyWith(fontWeight: FontWeight.w600);
+  static TextStyle bold = base.copyWith(fontWeight: FontWeight.w700);
+  static TextStyle extraBold = base.copyWith(fontWeight: FontWeight.w800);
+  static TextStyle black = base.copyWith(fontWeight: FontWeight.w900);
+}
+
+class FontSatoshi {
+  static const TextStyle base =
+      TextStyle(fontFamily: 'Satoshi', package: 'betty_resource');
+
+  static TextStyle light = base.copyWith(fontWeight: FontWeight.w300);
+  static TextStyle regular = base.copyWith(fontWeight: FontWeight.w400);
+  static TextStyle medium = base.copyWith(fontWeight: FontWeight.w500);
+  static TextStyle semiBold = base.copyWith(fontWeight: FontWeight.w600);
+  static TextStyle bold = base.copyWith(fontWeight: FontWeight.w700);
+  static TextStyle extraBold = base.copyWith(fontWeight: FontWeight.w800);
+  static TextStyle black = base.copyWith(fontWeight: FontWeight.w900);
+}
+
+class FontInter {
+  static const TextStyle base =
+      TextStyle(fontFamily: 'Inter', package: 'betty_resource');
+
+  static TextStyle light = base.copyWith(fontWeight: FontWeight.w300);
+  static TextStyle regular = base.copyWith(fontWeight: FontWeight.w400);
+  static TextStyle medium = base.copyWith(fontWeight: FontWeight.w500);
+  static TextStyle semiBold = base.copyWith(fontWeight: FontWeight.w600);
+  static TextStyle bold = base.copyWith(fontWeight: FontWeight.w700);
+  static TextStyle extraBold = base.copyWith(fontWeight: FontWeight.w800);
+  static TextStyle black = base.copyWith(fontWeight: FontWeight.w900);
+}
