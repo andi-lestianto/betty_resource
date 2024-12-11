ede832691a82b796d9c22894b0382e2b5fb44f58
andi-lestianto <andi7les@gmail.com>
2024-12-11 19:35:58 +0700

    style : added color utils

---
+++ lib/colorutils/bettycolor_utils.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class GeneralColor {
+  static Color clrF3F3F3BgApp = const Color(0xFFF3F3F3);
+  static Color clrFFFFFFWhite = const Color(0xFFFFFFFF);
+  static Color clrFFFFFFWhiteGray = const Color(0xFFFAFAFA);
+  static Color clrF4F4F4White = const Color(0xFFF4F4F4);
+  static Color clrD1D0D3Gray = const Color(0xFFD1D0D3);
+  static Color clr464646DarkGray = const Color(0xFF464646);
+  static Color clr646465GrayFraction = const Color(0xFF646465);
+  static Color clr292D32GrayCircle = const Color(0xFF292D32);
+  static Color clrF5F5F5BgGray = const Color(0xFFF5F5F5);
+  static Color clr8A8A8AGrayBtn = const Color(0xFF8A8A8A);
+  static Color clrDDDDDDGray = const Color(0xFFDDDDDD);
+
+  static Color clr00B9AEBlue = const Color(0xFF00B9AE);
+  static Color clr2F80EDBlue = const Color(0xFF2F80ED);
+  static Color clr0099FFFlowkitBlue = const Color(0xFF0099FF);
+  static Color clrEBFDFFBlueBG = const Color(0xFFEBFDFF);
+  static Color clrF5FFFEBlueTrans = const Color(0xFFF5FFFE);
+
+  static Color clrF02D3ARed = const Color(0xFFF02D3A);
+  static Color clrFEF5F6RedTrans = const Color(0xFFFEF5F6);
+  static Color clr8F0A13RedMaroon = const Color(0xFF8F0A13);
+  static Color clrF17300Orange = const Color(0xFFF17300);
+  static Color clrFFF2E9OrangeWhite = const Color(0xFFFFF2E9);
+  static Color clrFFF6F0OrangeWhite = const Color(0xFFFFF6F0);
+  static Color clrFBFDF6GreenTrans = const Color(0xFFFBFDF6);
+
+  static Color clr110D17BlackTxt = const Color(0xFF110D17);
+  static Color clr98949EGrayTxt = const Color(0xFF98949E);
+  static Color clrTeal = const Color(0XFF6FBAB0);
+  static Color clr222222Charcoal = const Color(0xFF222222);
+
+  static List<Color> clrGradientGreentty = [
+    const Color(0xFF38EF7D),
+    const Color(0xFF11998E)
+  ];
+  static List<Color> clrGradientPurpletty = [
+    const Color(0xFFE100FF),
+    const Color(0xFF7F00FF)
+  ];
+  static List<Color> clrGradientOrangetty = [
+    const Color(0xFFEEA849),
+    const Color(0xFFF46B45)
+  ];
+  static List<Color> clrGradientBluetty = [
+    const Color(0xFF56CCF2),
+    const Color(0xFF2F80ED)
+  ];
+
+  static List<Color> clrGradientSoftGreen = [
+    const Color(0xFFFFFFFF),
+    const Color(0xFFBFFBDE)
+  ];
+
+  static List<Color> clrGradientBlack = [
+    const Color(0xFF414345),
+    const Color(0xFF232526)
+  ];
+  static List<Color> clrGradientRedtty = [
+    const Color(0xFFEF473A),
+    const Color(0xFFCB2D3E)
+  ];
+  static List<Color> clrGradAuroraBlue = [
+    const Color(0XFF00B9AE),
+    const Color(0XFF003068)
+  ];
+  static List<Color> clrGradieintWhite = [
+    const Color(0XFFFBFDF6),
+    const Color(0XFFFBFDF6)
+  ];
+
+  static Color clr005B41green = const Color(0xFF005B41);
+  static Color clr008170Green = const Color(0xFF008170);
+  static Color clrA8DCBCGreen = const Color(0xFFA8DCBC);
+
+  static List<Color> clrGradientGreen = [clr005B41green, clr008170Green];
+
+  static List<Color> clrGradientGreen2 = [
+    clr005B41green,
+    clr008170Green,
+    clr008170Green
+  ];
+
+  static Color clr00B9AEDropShadow = const Color(0xFF00B9AE).withOpacity(0.05);
+
+  //PieChart
+  static Color clr8DECB4lightGreen = const Color(0xFF8DECB4);
+  static Color clr41B06EdarkerGreen = const Color(0xFF41B06E);
+  static Color clrE6FF94lighterGreen = const Color(0xFFE6FF94);
+
+  //TootlipsBG
+  static Color clr282828BgTooltips = const Color(0xFF282828);
+}
