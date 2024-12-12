a5d7f4c87ff1ae594f3a1a20b4594f18d1be7f55
andi-lestianto <andi7les@gmail.com>
2024-12-12 14:26:49 +0700

    style : add onboarding image

--- lib/imageutils/bettyimage_utils.dart
+++ lib/imageutils/bettyimage_utils.dart
@@ -1,29 +1,67 @@
 class BettyImage {
   static const String path = 'packages/betty_resource/';
 
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-ellips1.png?raw=true)
   static const imgEllips1Png = '${path}assets/image/img-ellips1.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-ellips2.png?raw=true)
   static const imgEllips2Png = '${path}assets/image/img-ellips2.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-googleplay.png?raw=true)
   static const imgGooglePlayPng = '${path}assets/image/img-googleplay.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-maincart-greendot.png?raw=true)
   static const imgMaincartGreenDotPng =
       '${path}assets/image/img-maincart-greendot.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-box-reminder.png?raw=true)
   static const imgBoxReminderPng = '${path}assets/image/img-box-reminder.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-payment-greendot.png?raw=true)
   static const imgPaymentGreenDotPng =
       '${path}assets/image/img-payment-greendot.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-payment-orangedot.png?raw=true)
   static const imgPaymentOrangeDotPng =
       '${path}assets/image/img-payment-orangedot.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-texture.png?raw=true)
   static const imgTexturePng = '${path}assets/image/img-texture.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-onboarding-greendot.png?raw=true)
   static const imgOnboardingGreenDotPng =
       '${path}assets/image/img-onboarding-greendot.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-tutorialdialog-bgintro.png?raw=true)
   static const imgTutorialDialogBgIntroPng =
       '${path}assets/image/img-tutorialdialog-bgintro.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-tutorialdialog-black-bottom.png?raw=true)
   static const imgTutorialDialogBlackBottomPng =
       '${path}assets/image/img-tutorialdialog-black-bottom.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-tutorialdialog-black-left.png?raw=true)
   static const imgTutorialDialogBlackLeftPng =
       '${path}assets/image/img-tutorialdialog-black-left.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-tutorialdialog-black-right.png?raw=true)
   static const imgTutorialDialogBlackRightPng =
       '${path}assets/image/img-tutorialdialog-black-right.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-tutorialdialog-bottom-left.png?raw=true)
   static const imgTutorialDialogBottomLeftPng =
       '${path}assets/image/img-tutorialdialog-bottom-left.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-tutorialdialog-top-right.png?raw=true)
   static const imgTutorialDialogTopRightPng =
       '${path}assets/image/img-tutorialdialog-top-right.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-onboarding1.png?raw=true)
+  static const imgOnboarding1Png = '${path}assets/image/img-onboarding1.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-onboarding2.png?raw=true)
+  static const imgOnboarding2Png = '${path}assets/image/img-onboarding2.png';
+
+  /// ![Preview](https://github.com/andi-lestianto/flutterimage-librarytest/blob/main/assets/image/img-onboarding3.png?raw=true)
+  static const imgOnboarding3Png = '${path}assets/image/img-onboarding3.png';
 }
