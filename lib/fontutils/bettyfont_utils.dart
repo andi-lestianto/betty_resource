9faa5eb819e7f124e165e90e4845e2973f049b5a
andi-lestianto <andi7les@gmail.com>
2024-12-27 14:24:14 +0700

    refactor: rename variable for fontsfpro

--- lib/fontutils/bettyfont_utils.dart
+++ lib/fontutils/bettyfont_utils.dart
@@ -40,16 +40,34 @@ class FontInter {
 }
 
 class FontSFPro {
+  /// SF Pro - Base - 400
   static const TextStyle base =
       TextStyle(fontFamily: 'SF-Pro', package: 'betty_resource');
 
-  static TextStyle baseThin = base.copyWith(fontWeight: FontWeight.w100);
-  static TextStyle baseUltralight = base.copyWith(fontWeight: FontWeight.w200);
-  static TextStyle baseLight = base.copyWith(fontWeight: FontWeight.w300);
-  static TextStyle baseRegular = base.copyWith(fontWeight: FontWeight.w400);
-  static TextStyle baseMedium = base.copyWith(fontWeight: FontWeight.w500);
-  static TextStyle baseSemibold = base.copyWith(fontWeight: FontWeight.w600);
-  static TextStyle baseBold = base.copyWith(fontWeight: FontWeight.w700);
-  static TextStyle baseHeavy = base.copyWith(fontWeight: FontWeight.w800);
-  static TextStyle baseBlack = base.copyWith(fontWeight: FontWeight.w900);
+  /// SF Pro - Thin - 100
+  static TextStyle thin = base.copyWith(fontWeight: FontWeight.w100);
+
+  /// SF Pro - Ultra Light - 200
+  static TextStyle ultralight = base.copyWith(fontWeight: FontWeight.w200);
+
+  /// SF Pro - Light - 300
+  static TextStyle light = base.copyWith(fontWeight: FontWeight.w300);
+
+  /// SF Pro - Regular - 400
+  static TextStyle regular = base.copyWith(fontWeight: FontWeight.w400);
+
+  /// SF Pro - Medium - 500
+  static TextStyle medium = base.copyWith(fontWeight: FontWeight.w500);
+
+  /// SF Pro - Semi Bold - 600
+  static TextStyle semiBold = base.copyWith(fontWeight: FontWeight.w600);
+
+  /// SF Pro - Bold - 700
+  static TextStyle bold = base.copyWith(fontWeight: FontWeight.w700);
+
+  /// SF Pro - Extra Bold - 800
+  static TextStyle extraBold = base.copyWith(fontWeight: FontWeight.w800);
+
+  /// SF Pro - Black - 900
+  static TextStyle black = base.copyWith(fontWeight: FontWeight.w900);
 }
