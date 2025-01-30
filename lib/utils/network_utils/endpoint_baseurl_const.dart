87cab5cbc6e0e084e9887fa9bc790d2622044e07
Anggi Wahyu Utomo <anggiwahyu543@gmail.com>
2025-01-30 14:52:07 +0700

    feat: add new endpoint

--- lib/utils/network_utils/endpoint_baseurl_const.dart
+++ lib/utils/network_utils/endpoint_baseurl_const.dart
@@ -2,5 +2,13 @@ class EndpointBaseUrl {
   // static const String baseUrl = 'https://api.betty.id';
   static const String baseUrl = 'http://36.90.208.144/dev-main-be';
   static const String apiUrl = '$baseUrl/api/v1/';
+
+  // Images
   static const String imageUrl = '$baseUrl/api/v1/images/';
+
+  // Session
+  static const String sessions = 'sessions';
+  static const String start = '$sessions/start';
+  static const String recap = '$sessions/recap';
+  static const String close = '$sessions/close';
 }
