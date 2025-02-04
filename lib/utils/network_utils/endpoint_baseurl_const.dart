2ce2db8633303bea12d5c8ba159d62b9e1131b94
Anggi Wahyu Utomo <anggiwahyu543@gmail.com>
2025-02-04 09:28:03 +0700

    fix: update baseUrl

--- lib/utils/network_utils/endpoint_baseurl_const.dart
+++ lib/utils/network_utils/endpoint_baseurl_const.dart
@@ -1,6 +1,6 @@
 class EndpointBaseUrl {
   // static const String baseUrl = 'https://api.betty.id';
-  static const String baseUrl = 'http://36.90.209.8/dev-main-be';
+  static const String baseUrl = 'http://36.90.208.84/dev-main-be';
   static const String apiUrl = '$baseUrl/api/v1/';
 
   // Images
